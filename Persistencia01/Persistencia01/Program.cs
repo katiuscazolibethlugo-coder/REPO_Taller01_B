3937770cb04dc7a3593f2ff486e5f7d96238f680
katiuscazolibethlugo <katiuscazolibethlugo@gmail.com>
2026-04-17 14:54:19 -0400

    creacion de repositorios

--- Persistencia01/Persistencia01/Program.cs
+++ Persistencia01/Persistencia01/Program.cs
@@ -1,12 +1,13 @@
 ﻿/*
  * Creado por SharpDevelop.
- * Usuario: Usuario
+ * Usuario: katiusca lugo
  * Fecha: 17/4/2026
  * Hora: 2:16 p. m.
  * 
  * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
  */
 using System;
+using System.IO;
 
 namespace Persistencia01
 {
@@ -16,9 +17,23 @@ namespace Persistencia01
 		{
 			Console.WriteLine("=== TALLER SECCION B  ");
 			
-			// TODO: Implement Functionality Here
+			// directorio
 			
-			Console.Write("Press any key to continue . . . ");
+			string rutaRaiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DATOSIUJO");
+			
+			string rutaReportes = Path.Combine(rutaRaiz, "REPORTES");
+			
+			Console.WriteLine(rutaRaiz);
+			Console.WriteLine(rutaReportes);
+			
+			if(!Directory.Exists(rutaReportes)){
+				//crear directorio reportes
+				Directory.CreateDirectory(rutaReportes);
+				Console.WriteLine("Directorio creado correctamente");
+			}
+				
+			//
+			Console.WriteLine("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
 	}
